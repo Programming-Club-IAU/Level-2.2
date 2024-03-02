fc7cfa25bdb60d16db53339513c5ec28de400f68
Talyia Mira <Tali.km.angel@gmail.com>
2024-03-02 23:07:38 +0300

    Complete

---
+++ src/guess.dart
@@ -0,0 +1,5 @@
+/* String getValidGuess() {
+-The getValidGuess function prompts the player to enter a guess.
+-It uses a loop to ensure that the input is a valid guess.
+-It reads the input from the user, trims and converts it to lowercase, and validates it using a regular expression.
+} */
---
+++ src/hangman.dart
@@ -0,0 +1,7 @@
+
+void hangman() {
+  print("Welcome to Hangman!");
+
+  //List<String> words = []; Start with a list of any words you wish to add
+  //choose random word from the list as the guessed word
+}
